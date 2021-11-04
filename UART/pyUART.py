27529f5c076ba44054a03d37b6a19797cd1ec5f0
JeremyCJWu <73487049+JeremyKTH@users.noreply.github.com>
2021-11-04 14:28:40 +0100

    Update pyUART.py

--- UART/pyUART.py
+++ UART/pyUART.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from WheelLoaderController3 import Controller, Observer
+from volvo.msg import state_message
 from UART import UART
 from SequenceExecuter import mapValue
 import sys
@@ -20,16 +21,17 @@ C = Controller(U)
 O = Observer(U)
 C.startThread()
 O.startThread()
-state_message=[]
+
 
 def Callback(message):
     global C, O
-    C.velocity = mapValue(float(message[0]), 0.2199, -0.2199)
-    C.steering_ang = mapValue(float(message[1]), 12.903, -12.903)
-    C.bucket_ang = mapValue(float(message[2]), 60, -46)
-    C.bucket_height = mapValue(float(message[3]), 477, -75)
-    C.scissor_ang = mapValue(float(message[4]), 35, 0)
+    C.velocity = mapValue(float(message.velocity), 0.2199, -0.2199)
+    C.steering_ang = mapValue(float(message.steering_angle), 12.903, -12.903)
+    C.bucket_ang = mapValue(float(message.bucket_angle), 60, -46)
+    C.bucket_height = mapValue(float(message.bucket_height), 477, -75)
+    C.scissor_ang = mapValue(float(message.scissor_angle), 35, 0)
     C.Run(3)
+    #print(message)
 
 
 if __name__ == '__main__':
@@ -37,5 +39,5 @@ if __name__ == '__main__':
     rospy.init_node('pyUART', anonymous = False)
 
     # Go to the main loop.
-    rospy.Subscriber('pySM', state_message, Callback)
+    rospy.Subscriber('pub_UART', state_message, Callback)
     rospy.spin()
