f6f40b0b3724ee30056556d7b07d5e798c510481
JeremyCJWu <73487049+JeremyKTH@users.noreply.github.com>
2021-11-04 14:27:55 +0100

    Update pySM.py

--- UART/pySM.py
+++ UART/pySM.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+from volvo.msg import state_message
 #import path planner ect. 
 
 
@@ -16,13 +17,28 @@ import rospy
     C.bucket_ang = mapValue(float(message[2]), 60, -46)
     C.bucket_height = mapValue(float(message[3]), 477, -75)
     C.scissor_ang = mapValue(float(message[4]), 35, 0)"""
-state_message= [ 0, 0 ,0 ,0 ,20]
+#state_message= [ 0, 0 ,0 ,0 ,20]
 
 if __name__ == '__main__':
     # Initialize the node and name it.
     rospy.init_node('pySM', anonymous = False)
-
+    
     # Go to the main loop.
-    rospy.Publisher('pyUART', state_message, queue_size=10)
-    rospy.Rate(10).sleep
-    rospy.spin()
+    pub_UART = rospy.Publisher('pub_UART', state_message, queue_size=10)
+    rospy.Rate(10)
+    
+    # Create message
+    state_msg = state_message()
+    state_msg.velocity = 0
+    state_msg.steering_angle = 0
+    state_msg.bucket_angle = 0
+    state_msg.bucket_height = 0
+    state_msg.scissor_angle = 20
+    
+    
+    
+    while not rospy.is_shutdown():
+        # Publish message
+        pub_UART.publish(state_msg)
+        rospy.Rate(10).sleep()
+   
