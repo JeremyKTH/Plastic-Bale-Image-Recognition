5ce68247698b9be6fb91e4538aad8b12db13e4dd
JeremyCJWu <73487049+JeremyKTH@users.noreply.github.com>
2021-11-15 11:44:36 +0100

    Update pyUART.py

--- UART/pyUART.py
+++ UART/pyUART.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+from codecs import xmlcharrefreplace_errors
 import rospy
-from WheelLoaderController3 import Controller, Observer
+from WheelLoaderController4 import Controller, Observer
 from volvo.msg import state_message
+from volvo.srv import cam_pos, cam_posResponse
 from UART import UART
 from SequenceExecuter import mapValue
 import sys
@@ -14,7 +16,7 @@ import sys
 # --------------------------------------------------------------------------------------------------
 # Note     : 
 """
-U = UART("COM4")
+U = UART("/dev/ttyTHS0")
 if(not U.running):
     sys.exit("Could not open serial port.")
 C = Controller(U)
@@ -23,21 +25,34 @@ C.startThread()
 O.startThread()
 
 
+def state_service():
+    global x, y
+    x *= 100
+    return cam_posResponse(x, y)
+                       
+
 def Callback(message):
-    global C, O
+    global C, O, x, y
     C.velocity = mapValue(float(message.velocity), 0.2199, -0.2199)
     C.steering_ang = mapValue(float(message.steering_angle), 12.903, -12.903)
     C.bucket_ang = mapValue(float(message.bucket_angle), 60, -46)
     C.bucket_height = mapValue(float(message.bucket_height), 477, -75)
     C.scissor_ang = mapValue(float(message.scissor_angle), 35, 0)
     C.Run(3)
-    #print(message)
+    
+    # Get states
+    x, y, _, _, _, _, _, _, _, _, _, _ = O.getStates()
+    
+    
 
 
 if __name__ == '__main__':
-    # Initialize the node and name it.
-    rospy.init_node('pyUART', anonymous = False)
 
-    # Go to the main loop.
-    rospy.Subscriber('pub_UART', state_message, Callback)
+    rospy.init_node('pyUART', anonymous = False)
+    
+    # Create a service for getting system states
+    s = rospy.Service('state_service', cam_pos, state_service)
+    
+    rospy.Subscriber('py_UART', state_message, Callback)
+    
     rospy.spin()
